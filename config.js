5e0943aeafb65cc2282986d2dafa752713bd6a38
Klaudia <kklaudiaw@gmail.com>
2020-03-27 10:18:25 +0100

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:NbG65IYgQ3fGmbxz@cluster0-qjd3z.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24*60*60*1000,
 }
